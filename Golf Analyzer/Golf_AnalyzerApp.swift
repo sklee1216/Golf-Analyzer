b1a75801250ee254f7eabac150b9ca7cfc419b8a
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2023-09-07 11:56:13 -0400

    Initial Commit

---
+++ Golf Analyzer/Golf_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_AnalyzerApp.swift
+//  Golf Analyzer
+//
+//  Created by Seungkeun Lee on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Golf_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
